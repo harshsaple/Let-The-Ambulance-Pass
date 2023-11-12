642ce63aa192079ca1b52ff56361376f8ae06d84
Harsh Saple <harshsaple.hs@gmail.com>
2023-11-11 23:16:51 -0800

    Code to return the co-ordinates to track the routes

--- accessToken.py
+++ accessToken.py
@@ -17,11 +17,19 @@ def accessToken():
     token = json.loads(data.decode('utf-8'))['result']['token']
     return token
 
+#Finds the route
 def find_route(headers,req):
     conn.request("GET",req,payload,headers)
     res = conn.getresponse()
     data = res.read()
     return json.loads(data.decode('utf-8'))
+
+#Get Routes from the find Route
+def get_route(headers,req):
+    conn.request("GET",req,payload,headers)
+    res = conn.getresponse()
+    data = res.read()
+    return json.loads(data.decode('utf-8'))['result']['trip']['routes'][0]['points']['coordinates']
 
 if __name__ == "__main__":
     accessToken()
