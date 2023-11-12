5131bd8886d4b3e64014af6ea76acefbe9362305
Yugandhar Desai <yugandhar.d.desai@gmail.com>
2023-11-11 18:59:20 -0800

    Added place names suggestion dropdown

--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
+
+
+
 function initMap() {
   var centerCoordinates = { lat: 37.7749, lng: -122.4194 };
 
@@ -19,7 +22,72 @@ function initMap() {
   });
 }
 
-function findRoute() {
+function sendPlaceNames() {
   // Add your logic for finding and displaying the route
   console.log("Initiating Emergency Rerouting...");
+  var start_place = document.getElementById("placeInput1").value;
+  var end_place = document.getElementById("placeInput2").value();
+
+  var data = {
+    startPlace: start_place,
+    endPlace: end_place
+  };
+  console.log(data)
+  // Make a POST request to the backend endpoint
+  fetch('/place-names', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      // Add any other headers you may need
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response from the server
+      console.log('Server response:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
 }
+function searchPlaces(inputId, dropdownId) {
+  var input = document.getElementById(inputId).value;
+
+  if (input.length < 3) {
+    $('#' + dropdownId).hide();
+    return;
+  }
+
+  $.ajax({
+    url: '/search_places',
+    type: 'GET',
+    data: { 'query': input },
+    success: function (response) {
+      var dropdown = $('#' + dropdownId);
+      dropdown.empty();
+      dropdown.show();
+
+      response.forEach(function (place) {
+        dropdown.append($('<option></option>').attr('value', place.place_id).text(place.name));
+      });
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  });
+}
+
+function selectPlace(dropdownId, inputId) {
+  var selectedPlace = $('#' + dropdownId).val();
+  var selectedText = $('#' + dropdownId + ' option:selected').text();
+  $('#' + inputId).val(selectedText);
+  $('#' + dropdownId).hide();
+  if (selectedPlace) {
+    console.log('Place selected:', selectedPlace);
+
+  }
+}
+
+
--- template.py
+++ template.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
+import os
+from geocoding import get_lat_long
+from models import Place
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
@@ -17,6 +21,62 @@ def json_demo():
         "end Point" :"+0987654"
     }
     return d
+
+@app.route('/search_places', methods=["GET", "POST"])
+def search_places(location=None, radius=5000):
+    api_key = os.environ.get('GOOGLE_API_KEY')
+    query = request.args.get('query')
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+
+    params = {
+        "query": query,
+        "key": api_key
+    }
+
+    if location:
+        params["location"] = location
+        params["radius"] = radius
+
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        places = response.json().get('results', [])
+        simplified_results = [{'name': place['name'], 'place_id': place['place_id']} for place in places]
+        return jsonify(simplified_results)
+    else:
+        return jsonify([])
+
+@app.route('/select_place', methods=['POST'])
+def select_place():
+    data = request.json
+    place_name = data.get('place_name')
+    lat, long = get_lat_long(place_name)
+    print(f'{place_name} : {lat}, {long}')
+    
+@app.route('/place-names', methods=['POST'])
+def get_place_names():
+    data = request.get_json()
+    try:
+        start_place, end_place = data['start_place'], datap['end_place']
+        print(start_place, end_place)
+        response_data = {'message': 'Route found successfully'}
+        
+        start_lat, start_long = get_lat_long(start_place)
+        end_lat, end_long = get_lat_long(end_place)
+        
+        start_place = Place(name=start_place, latitiude=start_lat, longitude=start_long)
+        end_place = Place(name=end_place, latitude=end_lat, longitude=end_long)
+        find_routes(start_place, end_place)
+        return jsonify(response_data), 200
+
+    except Exception as e:
+        # Handle exceptions appropriately
+        error_message = str(e)
+        print("Error:", error_message)
+        return jsonify({'error': error_message}), 500
+
+@app.route('/find-routes', methods=['GET', 'POST'])
+def find_routes(start_place : Place, end_place : Place):
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
