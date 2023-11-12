8fe9a2c3f19c1d973ffd2f6a9a857d85173d44e8
Swapnil Behere <swapnilbehere10@gmail.com>
2023-11-11 21:38:00 -0800

    API calls

---
+++ accessToken.py
@@ -0,0 +1,27 @@
+# import http.client
+# import json
+# token = 0
+# conn = http.client.HTTPConnection("api.iq.inrix.com")
+# payload = ''
+# headers = {}
+import http.client
+import json
+conn = http.client.HTTPSConnection("api.iq.inrix.com")
+payload = ''
+headers = {}
+req = ''
+def accessToken():
+    conn.request("GET", "/auth/v1/appToken?appId=t0cmg5z6u2&hashToken=dDBjbWc1ejZ1MnxXVExmZGU4ZFliN3FzUzlGYmZkazgzWUFnRlRNMTRkRG53azJYVHhp", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    token = json.loads(data.decode('utf-8'))['result']['token']
+    return token
+
+def find_route(headers,req):
+    conn.request("GET",req,payload,headers)
+    res = conn.getresponse()
+    data = res.read()
+    return json.loads(data.decode('utf-8'))
+
+if __name__ == "__main__":
+    accessToken()
